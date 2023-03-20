758a4fa3669ab2edaf09f87784aa55c53e7045d4
Petr Bednář <p.petr.bednar@gmail.com>
2023-03-21 00:12:57 +0100

    Cleaning

--- src/main/java/io/github/ppetrbednar/stp/AppLauncher.java
+++ src/main/java/io/github/ppetrbednar/stp/AppLauncher.java
@@ -1,14 +1,7 @@
 package io.github.ppetrbednar.stp;
 
-
-import io.github.ppetrbednar.stp.logic.STProfiler;
-
 public class AppLauncher {
     public static void main(String[] args) {
         App.main(args);
-
-       /* STProfiler profiler = new STProfiler();
-       // profiler.profile();
-        profiler.testPrint();*/
     }
 }
